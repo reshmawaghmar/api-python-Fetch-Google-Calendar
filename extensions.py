7f98ea91098bffe32d434f4787d1cd345db65b13
Reshma Waghmare <reshmawaghmare007@gmail.com>
2022-08-23 18:54:38 +0530

    PYT-27-feat-fethed-google-calendar
    fetched google calendar events

--- extensions.py
+++
@@ -1,5 +0,0 @@
-from flask import (
-    Flask, Response, request
-)
-
-app = Flask(__name__)
--- main2.py
+++ main2.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
-from flask import Flask
+
 import datetime
+import imp
 import os.path
 
 from google.auth.transport.requests import Request
@@ -8,26 +9,18 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from flask import Flask, jsonify
 
-
-app= Flask(__name__)
-
-
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+app = Flask(__name__)
 
+
+@app.route('/')
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -35,15 +28,13 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
         service = build('calendar', 'v3', credentials=creds)
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        now = datetime.datetime.utcnow().isoformat() + 'Z' 
         print('Getting the upcoming 10 events')
         events_result = service.events().list(calendarId='primary', timeMin=now,
                                               maxResults=10, singleEvents=True,
@@ -54,14 +45,14 @@ def main():
             print('No upcoming events found.')
             return
 
-        # Prints the start and name of the next 10 events
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
             print(start, event['summary'])
 
     except HttpError as error:
         print('An error occurred: %s' % error)
+    return jsonify({"data": events_result})
 
 
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
